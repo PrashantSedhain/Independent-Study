ccbbf14718dea12a17ad6ceefb1c712bcb83a4a5
Prashant Sedhain <prashantased@gmail.com>
2020-05-27 18:55:24 -0500

    Added test component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { SplitPageComponent } from "./split-page/split-page.component";
 import { InputPageComponent } from "./input-page/input-page.component";
 import { FinishPageComponent } from "./finish-page/finish-page.component";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { TestComponent } from "./test/test.component";
 
 const routes: Routes = [
   { path: "splitPage", component: SplitPageComponent },
   { path: "", component: LandingPageComponent },
   { path: "inputPage", component: InputPageComponent },
   { path: "finishPage", component: FinishPageComponent },
+  { path: "test", component: TestComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { SplitPageComponent } from "./split-page/split-page.component";
 import { InputPageComponent } from "./input-page/input-page.component";
 import { FinishPageComponent } from "./finish-page/finish-page.component";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { LandingPageComponent } from "./landing-page/landing-page.component";
     InputPageComponent,
     FinishPageComponent,
     LandingPageComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
