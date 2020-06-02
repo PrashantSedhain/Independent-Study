1dd91905500ab2dd1838a68bf0143ecc8a315a39
Prashant Sedhain <prashantased@gmail.com>
2020-06-02 18:54:01 -0500

    Added login functionality

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
+var _ = require("lodash");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
@@ -25,8 +26,51 @@ router.post("/signup", (req, res, next) => {
   });
 });
 
-router.get("/test", (req, res, next) => {
-  res.status(200).json({ message: "Passed" });
+router.post("/login", loginCaseChange, (req, res, next) => {
+  let getUser;
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "User with that email not found!",
+        });
+      }
+      getUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Invalid Password!",
+        });
+      }
+      const token = jwt.sign(
+        {
+          id: getUser._id,
+        },
+        "secret_password_here_this_is_Temporary",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600,
+      });
+    })
+    .catch((err) => {
+      return Promise.resolve();
+    });
 });
+
+function loginCaseChange(req, res, next) {
+  req.body.email = _.toLower(req.body.email);
+  next();
+}
+
+function changeCase(req, res, next) {
+  req.body.firstName = _.upperFirst(req.body.firstName);
+  req.body.lastName = _.upperFirst(req.body.lastName);
+  req.body.email = _.toLower(req.body.email);
+  next();
+}
 
 module.exports = router;
