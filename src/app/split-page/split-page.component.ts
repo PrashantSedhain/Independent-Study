6624b6f2612fe99499636cfa74c93c4831d82770
Prashant Sedhain <prashantased@gmail.com>
2020-06-18 13:21:46 -0500

    Changed to FormControl and it works now

--- src/app/split-page/split-page.component.ts
+++ src/app/split-page/split-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import {
   FormControl,
-  FormGroupDirective,
   NgForm,
   Validators,
   FormGroup,
+  FormArray,
+  FormBuilder,
 } from "@angular/forms";
 import { ErrorStateMatcher } from "@angular/material/core";
 import { CalculationService } from "../calculation.service";
@@ -16,6 +17,11 @@ import { groupBy } from "lodash";
 
 interface Test {
   userID: string;
+}
+
+interface Member {
+  name: string;
+  email: string;
 }
 
 interface Group {
@@ -32,26 +38,14 @@ interface Person {
   owes: {};
 }
 
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl | null,
-    form: FormGroupDirective | NgForm | null
-  ): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(
-      control &&
-      control.invalid &&
-      (control.dirty || control.touched || isSubmitted)
-    );
-  }
-}
 @Component({
   selector: "app-split-page",
   templateUrl: "./split-page.component.html",
   styleUrls: ["./split-page.component.css"],
 })
 export class SplitPageComponent implements OnInit {
+  memberData: Array<Member>;
+  submitted = false;
   groupForm: FormGroup;
   default: any;
   numbers: Array<Number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -60,12 +54,9 @@ export class SplitPageComponent implements OnInit {
     private snackBarService: SnackbarService,
     private router: Router,
     private groupService: GroupService,
-    private authService: AuthService
-  ) {
-    this.groupForm = new FormGroup({
-      numOfPeople: new FormControl(null),
-    });
-  }
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
   groupByID: Group;
   ListOfGroups: Array<Group>;
   userId: Test;
@@ -83,8 +74,12 @@ export class SplitPageComponent implements OnInit {
     Validators.email,
   ]);
 
-  matcher = new MyErrorStateMatcher();
   ngOnInit() {
+    this.groupForm = this.formBuilder.group({
+      numOfPeople: ["", Validators.required],
+      members: new FormArray([]),
+    });
+
     this.loading = true;
     var groupData = this.groupService.findGroups();
     groupData.subscribe((data) => {
@@ -120,6 +115,13 @@ export class SplitPageComponent implements OnInit {
 
     return true;
   }
+  get t() {
+    return this.groupForm.controls.members as FormArray;
+  }
+
+  get f() {
+    return this.groupForm.controls;
+  }
 
   createGroup() {
     var data = this.authService.getCurrentUserID();
@@ -135,6 +137,38 @@ export class SplitPageComponent implements OnInit {
     this.groupService.createGroup(jsonBody);
   }
 
+  onReset() {
+    // reset whole form back to initial state
+    this.submitted = false;
+
+    this.groupForm.reset();
+    this.t.clear();
+  }
+
+  onClear() {
+    // clear errors and reset ticket fields
+    this.submitted = false;
+
+    this.t.reset();
+  }
+  onChangeTickets(e) {
+    const numberOfTickets = e.target.value || 0;
+    this.selected = numberOfTickets;
+    if (this.t.length < numberOfTickets) {
+      for (let i = this.t.length; i < numberOfTickets; i++) {
+        this.t.push(
+          this.formBuilder.group({
+            name: ["", Validators.required],
+            email: ["", [Validators.required, Validators.email]],
+          })
+        );
+      }
+    } else {
+      for (let i = this.t.length; i >= numberOfTickets; i--) {
+        this.t.removeAt(i);
+      }
+    }
+  }
   findGroupByID(groupID) {
     var groupData = this.groupService.findGroupByID(groupID);
     this.loading = true;
@@ -143,9 +177,34 @@ export class SplitPageComponent implements OnInit {
       this.groupForm.controls["numOfPeople"].setValue(this.groupByID.count, {
         onlySelf: true,
       });
-      this.createArray();
+
+      if (this.t.length < this.groupByID.count) {
+        for (let i = this.t.length; i < this.groupByID.count; i++) {
+          if (this.t.length < this.groupByID.count) {
+            this.t.push(
+              this.formBuilder.group({
+                name: ["", Validators.required],
+                email: ["", [Validators.required, Validators.email]],
+              })
+            );
+          }
+        }
+      } else {
+        for (let i = this.t.length; i >= this.groupByID.count; i--) {
+          this.t.removeAt(i);
+        }
+      }
       this.loading = false;
     });
+  }
+
+  onSubmit() {
+    if (this.groupForm.invalid) {
+      alert("Form is invalid");
+      return;
+    }
+    this.parseForm();
+    this.addUser();
   }
 
   addUser() {
@@ -167,6 +226,14 @@ export class SplitPageComponent implements OnInit {
       this.calculationService.users = this.users;
       this.userId = this.authService.getCurrentUserID();
       this.router.navigate(["/inputPage"]);
+    }
+  }
+
+  parseForm() {
+    this.memberData = this.groupForm.get("members").value;
+    for (let i = 0; i < this.memberData.length; i++) {
+      this.nameArray[i] = this.memberData[i].name;
+      this.emailArray[i] = this.memberData[i].email;
     }
   }
 }
